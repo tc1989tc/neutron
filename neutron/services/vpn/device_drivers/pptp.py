tc1989tc/neutron

commit 9353ca177fc8a6316c8825e3e6d59984a3c47057

Enable kill pptpd process if router netns does not exist
Pptpd process will alive on l3 agent if l3 router
netnamespace destroyed prio to pptpd exit.

Fixes: #7694
Signed-off-by: cheng.tang <tangch318@gmail.com>

neutron/services/vpn/device_drivers/pptp.py
@@ -120,7 +120,17 @@ class PPTPProcess(object):
     def stop(self):
         self.enabled = False
         if self.active:
-            self._execute(['kill', '-9', '--', '-%s' % self.pid])
+            cmd = ['kill', '-9', '--', '-%s' % self.pid]
+            # check if namespace exists
+            ip_wrapper = ip_lib.IPWrapper(self.root_helper, self.namespace)
+            if ip_wrapper.netns.exists(self.namespace):
+                self._execute(cmd)
+            else:
+                LOG.info(_('Router netnamespace %(namespace)s not found, '
+                         'kill pptpd process id %(pid)s in root netnamespace'),
+                         {'namespace': self.namespace, 'pid': self.pid})
+                # just kill process in root netnamespace
+                utils.execute(cmd)
 
     def update_ports_status(self):
         changed = {}

commit 56c28cd23cc4db7a5872e1b97867ff03030f848d

Fix cannot delete pptpd process
Fixes: redmine #7991
Fixes: 9353ca1 ("Enable kill pptpd process if router netns does not exist")

Signed-off-by: cheng.tang <tangch318@gmail.com>

neutron/services/vpn/device_drivers/pptp.py
@@ -130,7 +130,7 @@ class PPTPProcess(object):
                          'kill pptpd process id %(pid)s in root netnamespace'),
                          {'namespace': self.namespace, 'pid': self.pid})
                 # just kill process in root netnamespace
-                utils.execute(cmd)
+                utils.execute(cmd, root_helper=self.root_helper)
 
     def update_ports_status(self):
         changed = {}