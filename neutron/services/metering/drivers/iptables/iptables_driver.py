1384ad755bce4ff29f8d2e8c2dfbca0adc4d17bd
Hunt Xu <mhuntxu@gmail.com>
2017-02-15 11:19:19 +0800

    Revert "metering: only modify related iptables parts"
    This reverts commit 54ad02a01ef463cb17bf90f532fe9b27b723e551.

--- neutron/services/metering/drivers/iptables/iptables_driver.py
+++ neutron/services/metering/drivers/iptables/iptables_driver.py
@@ -30,7 +30,7 @@ LOG = logging.getLogger(__name__)
 NS_PREFIX = 'qrouter-'
 WRAP_NAME = 'neutron-meter'
 EXTERNAL_DEV_PREFIX = 'qg-'
-TOP_CHAIN = "neutron-filter-top"
+TOP_CHAIN = WRAP_NAME + "-local"
 RULE = '-r-'
 LABEL = '-l-'
 
@@ -76,10 +76,6 @@ class RouterWithMetering(object):
             namespace=self.ns_name,
             binary_name=WRAP_NAME,
             use_ipv6=ipv6_utils.is_enabled())
-        # Clear tables/chains/rules that has nothing to do with metering
-        self.iptables_manager.ipv4 = {
-            'filter': iptables_manager.IptablesTable(binary_name=WRAP_NAME)}
-        self.iptables_manager.ipv6 = {}
         self.metering_labels = {}
 
 
